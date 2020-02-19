da85654a9299dacdc3c3581912df134cf55693e2
Scott Pletcher <spletch@umich.edu>
2020-02-19 17:48:34 -0500

    Added ability to link events across short durations

--- FLIC/TestingCode.R
+++ FLIC/TestingCode.R
@@ -20,4 +20,44 @@ PlotLicksandLight.Well(DFM11,8,range=c(102,103))
                         
 
 p<-ParametersClass.SingleWell()
-dfm<-DFMClass.LinkFiles(1,p)
+p<-SetParameter(p,Feeding.Event.Link.Gap=10000)
+dfm<-DFMClass(4,p)
+
+
+
+tmp<-dfm$LickData$W1
+
+
+
+Link.Events<-function(z,thresh){
+  zz<-z
+  tmp<-rle(z)
+  for(i in 1:(length(tmp$lengths)-1){
+    if(tmp$values[i]==FALSE && tmp$lengths[i]<thresh){
+      zz[tmp$lengths[i]:tmp$lengths[i+1]-1]<-TRUE
+    }
+  }
+  zz
+}
+
+Link.Events<-function(z,thresh){
+  tmp<-rle(z)
+  result<-c(FALSE)
+  for(i in 1:length(tmp$lengths)){
+    if(tmp$values[i]){
+      tmp2<-rep(TRUE,tmp$lengths[i])
+      result<-c(result,tmp2)
+    }
+    else {
+      if(tmp$lengths[i]>thresh){
+        tmp2<-rep(FALSE,tmp$lengths[i])
+        result<-c(result,tmp2)
+      }
+      else {
+        tmp2<-rep(TRUE,tmp$lengths[i])
+        result<-c(result,tmp2)
+      }
+    }
+  }
+  result[-1]
+}
